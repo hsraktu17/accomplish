fc47d56b77fb4a73fb97f41bdef14868a0fc200e
Utkarsh <78083867+hsraktu17@users.noreply.github.com>
2024-12-18 21:55:52 +0530

    backend removed

--- backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { config } from "dotenv";
-
-const app = express();
-config();
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => console.log(`server started at ${PORT}`));
